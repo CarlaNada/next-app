c448304ecd007fa42f5c69ab4ab7768c7d319894
Carla <c.nadaligdutto@itecriocuarto.org.ar>
2026-04-10 20:53:57 -0300

    Fin clase4 - Tema Layouts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +23,16 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <nav className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans bg-zinc-800">
+          <Link href={"/"}>Inicio</Link>
+          <Link href={"/notes"}>Notes</Link>
+          <Link href={"/notes/create"}>Create</Link>
+          <Link href={"/about"}>About</Link>
+        </nav>
+
+        {children}
+      </body>
     </html>
   );
 }
